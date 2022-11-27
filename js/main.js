802ab6ccb7f6e9646b80b0dcf8ff10d0c2e654d2
mohamed <m635379@gmail.com>
2022-11-27 23:14:08 +0200

    get api and put products in home page

--- js/main.js
+++ js/main.js
@@ -29,3 +29,36 @@ function logout(e) {
     }
   });
 }
+
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((data) => data)
+  .then((data) => {
+    let products = document.querySelector(".content .products");
+    let result = data.map((item) => {
+      return `
+      <div class="product col-sm-6 col-md-4 col-md-3">
+                    <img class="product-img" src="${item.image}" alt="${
+        item.title
+      }">
+                    <div class="product-info">
+                        <h2 class="product-heading">${item.title.slice(
+                          0,
+                          25
+                        )}</h2>
+                        <p class="product-desc">${item.description.slice(
+                          0,
+                          30
+                        )}</p>
+                        <p class="product-price">${item.price}</p>
+                    </div>
+                    <div class="product-actions">
+                        <button class="product-add-to-cart">Add to cart</button>
+                        <button class="favorite"><i class="fa-regular fa-heart"></i></button>
+                    </div>
+      `;
+    });
+    result.forEach((el) => {
+      products.innerHTML += el;
+    });
+  });
