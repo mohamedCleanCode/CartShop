29b3fb59e40b192a0c65ae5ca302db52c0ec804e
mohamed <m635379@gmail.com>
2022-11-28 22:42:20 +0200

    add some thing to project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
+// Start Cart
+
+let cartBadge = document.querySelector(".cart-badge");
+let cartContianer = document.querySelector(".cart-container");
+let closeCart = document.querySelector(".close-cart");
+let allProductsCart = document.querySelector(".all-products-cart");
+let totalPrice = document.querySelector(".total-price");
+let count = document.querySelector(".count");
+cartBadge.addEventListener("click", (e) => {
+  if (cartContianer.classList.contains("open-cart")) {
+    cartContianer.classList.toggle("open-cart");
+  } else {
+    cartContianer.classList.toggle("open-cart");
+  }
+});
+closeCart.addEventListener("click", () => {
+  cartContianer.classList.toggle("open-cart");
+});
+document.addEventListener("click", (e) => {
+  if (e.target != cartBadge && cartContianer.classList.contains("open-cart")) {
+    cartContianer.classList.remove("open-cart");
+  }
+});
+
+// End Cart
+
+// Start Main Logic
+
 let userInfo = document.querySelector(".user-info");
 let userAccount = document.querySelector(".user-info a");
 let links = document.querySelector(".links");
 let user = localStorage.getItem("username");
+let allProducts = [];
 
 if (user) {
   links.remove();
@@ -34,6 +63,7 @@ fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((data) => data)
   .then((data) => {
+    data.forEach((el) => allProducts.push(el));
     let products = document.querySelector(".content .products");
     let result = data.map((item) => {
       return `
@@ -72,6 +102,22 @@ function checkLogedInUser() {
     window.location.href = "login.html";
   }
 }
+
+// End Main Logic
+
 function addProductToCart(id) {
-  console.log(id);
+  let choose = allProducts.find((item) => item.id === id);
+  allProductsCart.innerHTML += `
+  <div class="cart-box">
+                                <img src="${choose.image}" alt="" class="cart-product-img">
+                                <div class="cart-product-details">
+                                    <div class="cart-product-title">${choose.title}</div>
+                                    <div class="cart-product-price">$${choose.price}</div>
+                                    <input type="number" value="1" class="cart-product-quantity">
+                                </div>
+                                <i class="fa-solid fa-trash cart-product-remove"></i>
+                            </div>
+  `;
+  count.style.display = "block";
+  count.innerHTML = allProductsCart.children.length;
 }
